441cf5c6b2996829db586879e158eaa174dbb7b9
Tsao <Tsao@Tsao.local>
2017-01-13 14:53:00 +0800

    添加socket文件

---
+++ modules/socket.js
@@ -0,0 +1,25 @@
+const SocketConst = {
+	Type: {
+		Broadcast: "Broadcast"
+	},
+	EventName: {
+		Update: "Update"
+	}
+}
+module.exports = {
+	const: SocketConst,
+	init: function(eventEmitter) {
+		var io = require('socket.io')();
+		io.on('connection', function(socket){
+			console.log('new client coming')
+		});
+
+		setInterval(function(){
+			eventEmitter.emit(SocketConst.Type.Broadcast, 'switch', {status: true});
+		},3000);
+
+		// 监听广播，并且向外发送
+		eventEmitter.on(SocketConst.Type.Broadcast, io.broadcast.emit);
+		io.listen(8800);
+	}
+}
