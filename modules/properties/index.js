c6eb24a664970137d5a3db3b47608485c24ebcbc
Tsao <Tsao@Tsao.local>
2017-01-14 22:58:06 +0800

    typo

--- modules/properties/index.js
+++ modules/properties/index.js
@@ -5,9 +5,9 @@
 var jsonfile = require('jsonfile');
 var filePath = __dirname + '/manifest.json';
 var deviceImplementation = require('./device_implementation');
-exports = module.exports = {
+module.exports = {
   manifest: jsonfile.readFileSync(filePath, {throws: false}),
-  implemtation: function (socket, propertyName, method,callback) {
+  implementation: function (socket, propertyName, method,callback) {
      var implementation = deviceImplementation[propertyName];
 
     implementation && implementation[method] && implementation[method](socket, callback);
